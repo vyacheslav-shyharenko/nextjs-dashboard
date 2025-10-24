8b13d1cbfcb5eacd0272dbc2a6fb4c86a809e15d
Vyacheslav Shyharenko <shyharenkovyacheslav.dev@gmail.com>
2025-10-24 23:16:22 +0300

    add next.js project

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import { inter } from "@/app/ui/fonts";
+import "./ui/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
